41f486cb97b924080701357d978de832432e0de5
Murali Veesambattu <MVeesambattu@simeiosolutions.com>
2018-12-22 15:55:43 +0530

    js Basics

---
+++ scripts/myScript.js
@@ -0,0 +1,75 @@
+document.getElementById("myH1").style.color="red";
+
+var a = 10;
+
+// alert(a)
+// document.write(a)
+// console.log(a)
+// document.getElementById("demo").innerHTML=a
+
+// alert("Helo")
+// prompt("Please Enter your Name");
+// confirm("Are you Happy ")
+
+var a = 100;
+
+var myMobile = {
+    name:"Coolpad",
+    price:10000
+}
+
+console.log(myMobile)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = 200;
+
+
+console.log(a)
